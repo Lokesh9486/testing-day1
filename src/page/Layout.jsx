ba6266a719133f3a2efa45058c84057faaef4dd6
Lokesh <lokesh.apr24@gmail.com>
2024-10-25 17:33:17 +0530

    demo1

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -6,10 +6,6 @@ import Input from "../components/Input";
 const Layout = ({ children }) => {
   return (
     <main>
-      {/*     "build:dev": "env-cmd -f .env.dev npm run test && env-cmd -f .env.dev npm run build",
-    "build:qa": "env-cmd -f .env.qa npm run test && env-cmd -f .env.qa npm run build",
-    "build:uat": "env-cmd -f .env.uat npm run test && env-cmd -f .env.uat npm run build",
-    "build:prod": "env-cmd -f .env.prod npm run test && env-cmd -f .env.prod npm run build", */}
       <Header />
       <Counter></Counter>
         <Input></Input>
